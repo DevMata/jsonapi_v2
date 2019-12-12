d2df52e7959a6f0cf87decab3b6ba7dd898cfbaf
Antonio Mata <hildeamp@gmail.com>
2019-12-12 17:48:03 -0600

    :recycle: Add router for tags

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import { blogs } from './src/routes/blogs';
+import { tags } from './src/routes/tags';
 import { connect } from './src/models/connection';
 
 connect();
@@ -9,5 +10,6 @@ const app = express();
 app.use(express.json());
 
 app.use('/blogs', blogs);
+app.use('/tags', tags);
 
 app.listen(3000, () => console.log('Listenting on port 3000'));
