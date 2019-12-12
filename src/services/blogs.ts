e88b293107fb9c4afc82b40c8cea35682bb9aef9
Antonio Mata <hildeamp@gmail.com>
2019-12-11 22:38:36 -0600

    :recycle: Refactor blogs service

--- src/services/blogs.ts
+++ src/services/blogs.ts
@@ -1,9 +1,12 @@
 import { Blog } from '../../models/blog';
-import { ApiResponse } from '../../controllers/api-response';
+import { ApiResponse } from './api-response';
+import { Blog as BlogForm } from '../../middleware/validations';
 
 export async function getBlogs(): Promise<ApiResponse> {
   try {
-    const blogs = await Blog.find();
+    const blogs = await Blog.find()
+      .sort({ createdAt: -1 })
+      .select('_id title content createdAt modifiedAt');
 
     return blogs.length
       ? { status: 200, response: { result: blogs } }
@@ -18,7 +21,7 @@ export async function getBlogs(): Promise<ApiResponse> {
 
 export async function getBlog(id: string): Promise<ApiResponse> {
   try {
-    const blog = await Blog.findById(id);
+    const blog = await Blog.findById(id).select('_id title content createdAt modifiedAt');
 
     return blog
       ? { status: 200, response: { result: blog } }
@@ -28,5 +31,42 @@ export async function getBlog(id: string): Promise<ApiResponse> {
       status: 421,
       response: { message: 'Cannot search the blog' }
     };
+  }
+}
+
+export async function deleteBlog(id: string): Promise<ApiResponse> {
+  try {
+    //blog must exist
+    const blog = await getBlog(id);
+    if (blog.status !== 200) {
+      return blog;
+    }
+
+    await Blog.findByIdAndDelete(id);
+
+    return {
+      status: 204,
+      response: {
+        message: 'Blog deleted'
+      }
+    };
+  } catch (error) {
+    return { status: 400, response: { message: 'Cannot delete blog' } };
+  }
+}
+
+export async function postBlog(blog: BlogForm): Promise<ApiResponse> {
+  try {
+    const newBlog = new Blog(blog);
+    const createdBlog = await newBlog.save();
+
+    return {
+      status: 201,
+      response: {
+        result: createdBlog
+      }
+    };
+  } catch (error) {
+    return { status: 421, response: { message: 'Cannot post blog' } };
   }
 }
