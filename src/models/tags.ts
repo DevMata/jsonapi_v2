47dffbd8b56205cfa9ebf9a2c437a45f1b66ca16
Antonio Mata <hildeamp@gmail.com>
2019-12-12 16:56:59 -0600

    :pencil2: Fix typo in tags model

--- src/models/tags.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const tagSchema = new mongoose.Schema({
-  name: String,
-  createdAt: { type: Date, default: Date.now() },
-  modifiedAt: { type: Date, default: null }
-});
-
-export const Tag = mongoose.model('Tag', tagSchema);
