5592872c7cc861abbdf642c2e9e6dbca309bfd91
Antonio Mata <hildeamp@gmail.com>
2019-12-12 11:07:15 -0600

    :recycle: Refactor main

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 
 import { blogs } from './routes/blogs';
-import { comments } from './routes/comments';
 import { connect } from './models/connection';
 
 connect();
@@ -10,6 +9,5 @@ const app = express();
 app.use(express.json());
 
 app.use('/blogs', blogs);
-app.use('/blogs/[0-9]+/comments', comments);
 
 app.listen(3000, () => console.log('Listenting on port 3000'));
