d32a39b24ce23e4d33cb7d4f9168144da96269d2
Antonio Mata <hildeamp@gmail.com>
2019-12-12 12:39:29 -0600

    :recycle: Write base of comments routes

--- routes/comments.ts
+++ routes/comments.ts
@@ -1,26 +1,31 @@
 import express from 'express';
-import { requireJson } from '../middleware/validations';
+import { requireJson, validateCommentId } from '../middleware/validations';
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 router.get('/', (req, res) => {
+  console.log('reached');
+  console.log(req.params.blog_id);
   res.end();
 });
 
-router.get('/:comment_id', (req, res) => {
-  res.end();
+router.get('/:comment_id', validateCommentId, (req, res) => {
+  res
+    .status(200)
+    .contentType('application/json')
+    .json(req.params);
 });
 
 router.post('/', requireJson, (req, res) => {
   res.end();
 });
 
-router.put('/:comment_id', requireJson, (req, res) => {
-  res.end();
-});
+// router.put('/:comment_id', requireJson, (req, res) => {
+//   res.end();
+// });
 
-router.delete('/:comment_id', (req, res) => {
-  res.end();
-});
+// router.delete('/:comment_id', (req, res) => {
+//   res.end();
+// });
 
 export { router as comments };
